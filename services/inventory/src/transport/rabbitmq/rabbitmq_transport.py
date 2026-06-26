ea4db51598550863b4ae327e5c0556d8a6940263
Serine <serine_md@hotmail.com>
2026-06-26 21:51:23 +0300

    Fixing inventory Rabbitmq transport listener

--- services/inventory/src/transport/rabbitmq/rabbitmq_transport.py
+++ services/inventory/src/transport/rabbitmq/rabbitmq_transport.py
@@ -1,5 +1,5 @@
 
-from typing import Optional
+from typing import List, Optional
 
 import aio
 import pika
@@ -10,7 +10,7 @@ from config.settings import settings
 
 class RabbitMQTransport(Listener, Producer):
 
-    def __init__(self, declare_exchange: bool = False, exchange_info: dict = None):
+    def __init__(self):
         self.listener_connection = None
         self.listener_channel = None
         self.producer_connection = None
@@ -18,33 +18,27 @@ class RabbitMQTransport(Listener, Producer):
         self.host = settings.rabbitmq_host
         self.port = settings.rabbitmq_port
         self.exchange = settings.rabbitmq_exchange
-        self.declare_exchange: bool = declare_exchange
-        self.exchange_info: dict = exchange_info
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host=self.host, port=self.port)
+        )
 
     def create_listener(self):
         """Create RabbitMQ listener connection and channel."""
-        self.listener_connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host=self.host)
-        )
+        self.listener_connection = self.connection
         self.listener_channel = self.listener_connection.channel()
 
-    def start_listener(self, queue_name: Optional[str] = None):
+    def initialize_listener(self, queue_name: Optional[str] = None):
+        """Initialize RabbitMQ listener by declaring the queue and binding it to the exchange."""
+        self.listener_channel.queue_declare(queue=queue_name, durable=True)
+        self.listener_channel.queue_bind(exchange=self.exchange, queue=queue_name)
+        
+    def start_listener(self, queue_names: Optional[List[str]] = None):
         """Start RabbitMQ listener to consume messages from the specified queue."""
-        if self.declare_exchange:
-            self.listener_channel.exchange_declare(exchange=self.exchange_info.get('name'), exchange_type=self.exchange_info.get('type'))
-            queue_name = self.listener_channel.queue_declare(queue = '', exclusive=True).method.queue
-            self.listener_channel.queue_bind(exchange = self.exchange_info.get('name'), queue = queue_name)
-            self.listener_channel.basic_qos(prefetch_count=1)  # Limit the number of unacknowledged messages to 1
+        self.listener_channel.basic_qos(prefetch_count=1)  # Limit the number of unacknowledged messages to 1
+        for queue_name in queue_names:
+            self.initialize_listener(queue_name)
             self.listener_channel.basic_consume(queue=queue_name, 
-                                           on_message_callback=self.callback, 
-                                           auto_ack=True) # Turned on manually but otherwise it's off by default
-
-        else:
-            self.listener_channel.queue_declare(queue=queue_name)
-            self.listener_channel.queue_bind(exchange = self.exchange, queue = queue_name)    
-            self.listener_channel.basic_qos(prefetch_count=1)  # Limit the number of unacknowledged messages to 1
-            self.listener_channel.basic_consume(queue=queue_name, 
-                                            on_message_callback=self.callback, 
+                                            on_message_callback=self.listener_callback, 
                                             auto_ack=True) # Turned on manually but otherwise it's off by default
         
         self.listener_channel.start_consuming()
@@ -55,9 +49,7 @@ class RabbitMQTransport(Listener, Producer):
 
     def create_producer(self):
         """Create RabbitMQ producer connection and channel."""
-        self.producer_connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host=self.host)
-        )
+        self.producer_connection = self.connection
         self.producer_channel = self.producer_connection.channel()
 
     def start_producer(self):
@@ -74,9 +66,13 @@ class RabbitMQTransport(Listener, Producer):
                                    routing_key=routing_key, 
                                    body=message)
     
-    def callback(self, ch, method, properties, body):
+    def listener_callback(self, ch, method, properties, body):
         """Callback function to handle incoming messages from the queue."""
         print(f" [x] Received {body}")
+    
+    def producer_callback(self, ch, method, properties, body):
+        """Callback function to handle outgoing messages to the queue."""
+        print(f" [x] Sending {body}")
         
 
     
